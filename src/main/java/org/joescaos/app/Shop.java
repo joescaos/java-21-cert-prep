2fd7f85fd64c7031d2ec6982213bf7f3452b00a7
Johan Esteban Cañas <johanestebancanas@192.168.1.12>
2025-07-22 20:34:21 -0500

    collections

--- src/main/java/org/joescaos/app/Shop.java
+++ src/main/java/org/joescaos/app/Shop.java
@@ -15,15 +15,19 @@ public class Shop {
   public static void main(String[] args) {
     ProductManager productManager = new ProductManager(Locale.US);
     Product p1 = productManager.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
-    productManager.printProductReport();
-    p1 = productManager.reviewProduct(p1, Rating.FOUR_STAR, "Nice coup of tea");
-    p1 = productManager.reviewProduct(p1, Rating.TWO_STAR, "Rather weak tea");
-    p1 = productManager.reviewProduct(p1, Rating.FOUR_STAR, "Fine tea");
-    p1 = productManager.reviewProduct(p1, Rating.FOUR_STAR, "Good tea");
-    p1 = productManager.reviewProduct(p1, Rating.THREE_STAR, "Just add some lemon");
-    productManager.printProductReport();
-//    Product p2 = productManager.createProduct(102, "Cake", BigDecimal.valueOf(1.98), Rating.FOUR_STAR,
-//            LocalDate.now().plusDays(2));
+    productManager.printProductReport(101);
+    productManager.reviewProduct(101, Rating.FIVE_STAR, "Nice coup of tea");
+    productManager.reviewProduct(101, Rating.TWO_STAR, "Rather weak tea");
+    productManager.reviewProduct(101, Rating.FOUR_STAR, "Fine tea");
+    productManager.reviewProduct(101, Rating.FOUR_STAR, "Good tea");
+    productManager.reviewProduct(101, Rating.THREE_STAR, "Just add some lemon");
+    productManager.printProductReport(101);
+    Product p2 = productManager.createProduct(102, "Cake", BigDecimal.valueOf(1.98), Rating.NOT_RATED,
+            LocalDate.now().plusDays(2));
+    productManager.printProductReport(102);
+    productManager.reviewProduct(102, Rating.FIVE_STAR, "Nice piece of cake");
+    productManager.reviewProduct(102, Rating.TWO_STAR, "too expensive");
+    productManager.printProductReport(102);
 //
 //    Product p3 = productManager.createProduct(103, "Coffee", BigDecimal.valueOf(1.98), Rating.FIVE_STAR);
 //
